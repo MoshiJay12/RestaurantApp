1609f505425230df754c75439794d63869e70bec
izzymoshi <izzymoshi42@gmail.com>
2023-01-22 18:14:49 +0800

    tmbh bill controller

---
+++ src/main/java/com/example/restaurantapp/BillController.java
@@ -0,0 +1,20 @@
+package com.example.restaurantapp;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.scene.input.*;
+import java.net.URL;
+
+import java.io.IOException;
+
+public class BillController {
+
+
+}
